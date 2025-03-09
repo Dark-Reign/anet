9af465d08007fbaf2d40e1c389c6e65cb86f627a
Harold Thetiot <hthetiot@gmail.com>
2025-03-09 23:29:05 +0100

    optimize docker/entrypoint.sh and docker/supervisord.conf

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/bash -e
 
 # Create local-servers.txt and populate it with an IP and hostname
+echo "- Create local-servers.txt..."
 HOSTNAME="${HOSTNAME:=localhost}"
-#export EXTERNAL_IP_EXEC_SET=`wget -qO- https://checkip.amazonaws.com`
-#echo "${EXTERNAL_IP_EXEC_SET} $HOSTNAME" > /home/alink/etc/local-servers.txt
-#cat /home/alink/etc/local-servers.txt
+export EXTERNAL_IP_EXEC_SET=`wget -qO- https://checkip.amazonaws.com`
+echo "${EXTERNAL_IP_EXEC_SET} $HOSTNAME" > /home/alink/etc/local-servers.txt
+cat /home/alink/etc/local-servers.txt
 
+touch /tmp/anet3srv.log
+touch /tmp/anet2cron.log
+
+echo "- Execute servfil..."
 cd ~/etc 
 ./servfil
 
 # Deploy public_html/etc
+echo "- Deploy public_html/etc"
 cd ~/public_html
-mkdir etc
+mkdir -p etc
 cd etc
 ln -s ~alink/etc/apps.txt apps.txt
 ln -s ~alink/etc/names.txt names.txt
@@ -19,9 +25,13 @@ ln -s ~alink/etc/servers.txt servers.txt
 ln -s ~alink/etc/types.txt types.txt
 
 # Deploy runsrvfil.cgi
+echo "- Deploy runsrvfil.cgi..."
 cd ~/public_html/etc
 cp ../../etc/runsrvfil.cgi .
 chmod ug+sx runsrvfil.cgi
+
+echo "- Start game server daemon..."
+~alink/etc/start2
 
 echo executing: "$@"
 exec "$@"
