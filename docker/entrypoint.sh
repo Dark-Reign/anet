186d195a73e893f2c39c08840a4180dd5fbd9f97
Harold Thetiot <hthetiot@gmail.com>
2025-03-09 20:34:55 +0100

    setup ~alink/public_html/ and start lighttpd

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,31 @@
 #!/bin/bash -e
 
-export EXTERNAL_IP_EXEC_SET="curl -s checkip.amazonaws.com"
+# Create local-servers.txt and populate it with an IP and hostname
+HOSTNAME="${HOSTNAME:=localhost}"
+#export EXTERNAL_IP_EXEC_SET=`wget -qO- https://checkip.amazonaws.com`
+#echo "${EXTERNAL_IP_EXEC_SET} $HOSTNAME" > /home/alink/etc/local-servers.txt
+#cat /home/alink/etc/local-servers.txt
 
-echo $EXTERNAL_IP_EXEC_SET
-
-cd /home/alink/etc 
+cd ~/etc 
 ./servfil
+
+# Deploy public_html/etc
+cd ~/public_html
+ln -s ~alink/public_html/ anet2
+mkdir etc
+cd etc
+ln -s ~alink/etc/apps.txt apps.txt
+ln -s ~alink/etc/names.txt names.txt
+ln -s ~alink/etc/servers.txt servers.txt
+ln -s ~alink/etc/types.txt types.txt
+
+# Deploy runsrvfil.cgi
+cd ~/public_html/etc
+cp ../../etc/runsrvfil.cgi .
+chmod ug+sx runsrvfil.cgi
+
+# Start lighttpd
+lighttpd -D -f /etc/lighttpd/lighttpd.conf&
 
 echo executing: "$@"
 exec "$@"
